fb15ee900f323a7bab8d10616f7e3f72ce6e59f4
Doug Torrance <dtorrance@monmouthcollege.edu>
2015-03-08 00:05:38 -0600

    Initial results of dh_make.

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(mpir):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="mpir" command="/usr/bin/mpir"
---
+++ debian/mpir.default.ex
@@ -0,0 +1,10 @@
+# Defaults for mpir initscript
+# sourced by /etc/init.d/mpir
+# installed at /etc/default/mpir by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
